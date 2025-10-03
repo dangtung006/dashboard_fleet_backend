01e628990bc7d87ceea91d125aea42667e8a3274
Tung Dang <tungds@vimo.vn>
2025-10-03 17:00:12 +0700

    update caller dto

--- src/dto/caller.py
+++ src/dto/caller.py
@@ -4,12 +4,12 @@ from typing import Optional
 
 
 class CallerBase(BaseModel, BaseDTO):
-    caller_name: str = Field(..., example="industries map")
+    caller_name: str = Field(..., example="Caller 1")
     caller_ip: str = Field(..., example="192.168.0.1")
-    caller_location: str = Field(..., example="HCM")
-    caller_desc: str = Field(..., example="industries map")
+    caller_location: str = Field(..., example="Location A")
+    caller_desc: str = Field(..., example="Caller in use for ...")
     caller_status: Optional[int] = 1
-    call_additional_date: str = Field(..., example="12/06/2025")
+    caller_additional_date: str = Field(..., example="12/06/2025")
 
 
 class CallerCreate(CallerBase):
